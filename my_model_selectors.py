bf4682393e5e427650cecfb1dc7bd4ffaf00fda8
Frederick Chyan <fchyan@gmail.com>
2017-09-04 00:43:13 -0700

    feat: custom features and selectors

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -76,9 +76,25 @@ class SelectorBIC(ModelSelector):
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
-
+        # implement model selection based on BIC scores
+        res = (None, None) # (BIC Score, Model)
+        for num_states in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                curr_model = self.base_model(num_states)
+                if curr_model is not None:
+                    curr_logL = curr_model.score(self.X, self.lengths)
+                    num_param_transition_probs = num_states * (num_states - 1)
+                    num_param_starting_probs = num_states - 1
+                    num_param_means = num_states * len(self.X[0]) # len(self.X[0]) is number of features
+                    num_param_covar = num_states * len(self.X[0])
+                    p = num_param_transition_probs + num_param_starting_probs + num_param_means + num_param_covar
+                    curr_BIC = -2 * curr_logL + p * math.log(len(self.lengths))
+                    if res[0] is None or curr_BIC < res[0]:
+                        res = (curr_BIC, curr_model)
+            except:
+                if self.verbose:
+                    print("failure on {} with {} states".format(self.this_word, num_states))
+        return res[1]
 
 class SelectorDIC(ModelSelector):
     ''' select best model based on Discriminative Information Criterion
@@ -92,9 +108,35 @@ class SelectorDIC(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        # implement model selection based on DIC scores
+        best_DIC = None
+        best_model = None
 
+        for num_states in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                curr_model = self.base_model(num_states)
+                sum_all_but_i = 0
+                m_minus_1 = 0
+                if curr_model is not None:
+                    for word, sequence in self.words.items():
+                        if word == self.this_word:
+                            continue
+                        word_X, word_Xlengths = self.hwords[word]
+                        try:
+                            word_logL = curr_model.score(word_X, word_Xlengths)
+                        except:
+                            continue
+                        sum_all_but_i += word_logL
+                        m_minus_1 += 1
+                    curr_logL = curr_model.score(self.X, self.lengths)
+                    curr_DIC = curr_logL - (1/m_minus_1) * sum_all_but_i
+                    if best_model == None or curr_DIC > best_DIC:
+                        best_DIC = curr_DIC
+                        best_model = curr_model 
+            except:
+                if self.verbose:
+                    print("failure on {} with {} states".format(self.this_word, num_states))
+        return best_model
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
@@ -104,5 +146,41 @@ class SelectorCV(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        # implement model selection using CV
+        if len(self.sequences) <= 2:
+            return self.base_model(self.n_constant)
+        best_num_components = None
+        max_k_logL = None
+        split_method = KFold()
+        for num_states in range(self.min_n_components, self.max_n_components + 1):
+            k_logL = None
+            for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                # print("Train fold indices:{} Test fold indices:{}".format(cv_train_idx, cv_test_idx))  # view indices of the folds
+                train_X = []
+                train_lengths = []
+                for i in cv_train_idx:
+                    train_X += self.sequences[i]
+                    train_lengths.append(len(self.sequences[i]))
+                test_X = []
+                test_lengths = []
+                for i in cv_test_idx:
+                    test_X += self.sequences[i]
+                    test_lengths.append(len(self.sequences[i]))
+                warnings.filterwarnings("ignore", category=DeprecationWarning)
+                try:
+                    hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
+                        random_state=self.random_state, verbose=False).fit(train_X, train_lengths)
+                    logL = hmm_model.score(test_X, test_lengths)
+                    if k_logL is None:
+                        k_logL = logL
+                    else:
+                        k_logL += logL
+                    if self.verbose:
+                        print("model created for {} with {} states".format(self.this_word, num_states))
+                except:
+                    if self.verbose:
+                        print("failure on {} with {} states".format(self.this_word, num_states))
+            if max_k_logL is None or k_logL is not None and k_logL > max_k_logL:
+                max_k_logL = k_logL
+                best_num_components = num_states
+        return self.base_model(best_num_components)
--- my_recognizer.py
+++ my_recognizer.py
@@ -20,6 +20,22 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    # implement the recognizer
+    # For every word_id in test_set, score it against each trained word model
+    # , store the log likelihood and also report the best word guess
+    for word_id, Xlengths in test_set.get_all_Xlengths().items():
+      curr_dict = dict()
+      best_guess = None
+      best_ll = None
+      for possible_word, possible_word_model in models.items():
+        try:
+          curr_ll = possible_word_model.score(Xlengths[0],Xlengths[1])
+        except:
+          continue
+        curr_dict[possible_word] = curr_ll
+        if not best_guess or curr_ll > best_ll:
+          best_guess = possible_word
+          best_ll = curr_ll
+      probabilities.append(curr_dict)
+      guesses.append(best_guess)
+    return probabilities, guesses
